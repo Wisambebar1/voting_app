99ee9008606d5b711aa911b610297cfc84ed138d
Wisambebar1 <wisambebar@gmail.com>
2024-11-15 14:10:17 +0200

    done setting up firebase

---
+++ voting_app/src/firebaseConfig.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyC0JpYwcxB1zsDt1JDHDMJC6bJtL-FxE-8",
+  authDomain: "voting-app-289a4.firebaseapp.com",
+  projectId: "voting-app-289a4",
+  storageBucket: "voting-app-289a4.firebasestorage.app",
+  messagingSenderId: "99916970021",
+  appId: "1:99916970021:web:a1238917f49e7b523aa7cc"
+};
+const app = initializeApp(firebaseConfig);
