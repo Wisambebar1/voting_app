fa49a6ec2727690745da98da434089a55af60b23
Wisambebar1 <wisambebar@gmail.com>
2024-11-15 18:55:08 +0200

    added hooks and deleted voting_page component because im not using it yet

--- voting_app/src/App.jsx
+++ voting_app/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 function App() {
-
+  const [user, setUser] = useState(null);
+  const [vote, setVote] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [view, setView] = useState('login');
   return (
     <div>
-      <Voting_page/>
     </div>
   )
 }
