22a5c56474564585ae2619e8114c50a55ca44b85
Wisambebar1 <wisambebar@gmail.com>
2024-11-15 11:48:41 +0200

    cleaned the react app and created the voting_page component,this is the begining of the form building, when im done all branches will be merged into this(main) branch in order to not get confused and let the name be true to its rank

---
+++ voting_app/src/Components/Voting_page.jsx
@@ -0,0 +1,10 @@
+const Voting_page=()=>{
+    return(
+        <div className="form-container">
+            <form action="/submit" method="POST">
+
+            </form>
+        </div>
+    )
+}
+export default Voting_page
