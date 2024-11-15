5262d269bef7813d48c71e7fb9e4fa0b737ecf39
Wisambebar1 <wisambebar@gmail.com>
2024-11-15 18:42:23 +0200

    deleted everything cuz i got lost, we will rally, regroup and try again

--- voting_app/src/App.jsx
+++ voting_app/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 import Voting_page from './Components/Voting_page'
-
+import LoginPage from './components/LoginPage';
+import Voting_Page from './components/VotingPage';
+import AdminPage from './components/AdminPage';
+import Spinner from './components/Spinner';
 function App() {
 
   return (
