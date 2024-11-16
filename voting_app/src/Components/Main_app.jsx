d6c90c5ced74949f66e9e907184fcac5aeee970c
Wisambebar1 <wisambebar@gmail.com>
2024-11-16 12:26:06 +0200

    imports

--- voting_app/src/Components/Main_app.jsx
+++ voting_app/src/Components/Main_app.jsx
@@ -1,1 +1,4 @@
 import { useUserContext } from "./Context";
+import LoginPage from './components/LoginPage';
+import VotingPage from './components/VotingPage';
+import AdminPage from './components/AdminPage';
