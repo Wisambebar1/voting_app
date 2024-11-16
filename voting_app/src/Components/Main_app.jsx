a583c9c10f93f7eab0e59b57eca50a588e50c62d
Wisambebar1 <wisambebar@gmail.com>
2024-11-16 12:27:10 +0200

    using the context

--- voting_app/src/Components/Main_app.jsx
+++ voting_app/src/Components/Main_app.jsx
@@ -2,3 +2,11 @@ import { useUserContext } from "./Context";
 import LoginPage from './components/LoginPage';
 import VotingPage from './components/VotingPage';
 import AdminPage from './components/AdminPage';
+const Main_app=()=>{
+    const { user, vote, isLoading, view, login, handleVote, changeVote, logout } = useUserContext();
+    return(
+        <div>
+
+        </div>
+    )
+}
