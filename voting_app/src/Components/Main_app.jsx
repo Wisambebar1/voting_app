4ed05606c2504c8d7fcefdd5425b9add21826ebc
Wisambebar1 <wisambebar@gmail.com>
2024-11-16 13:15:46 +0200

    done with main app component

--- voting_app/src/Components/Main_app.jsx
+++ voting_app/src/Components/Main_app.jsx
@@ -4,6 +4,24 @@ import VotingPage from './components/VotingPage';
 import AdminPage from './components/AdminPage';
 const Main_app=()=>{
     const { user, vote, isLoading, view, login, handleVote, changeVote, logout } = useUserContext();
+    switch (view) {
+        case 'login':
+          return <LoginPage login={login} />;
+        case 'vote':
+          return (
+            <VotingPage
+              user={user}
+              vote={vote}
+              handleVote={handleVote}
+              changeVote={changeVote}
+              isLoading={isLoading}
+            />
+          );
+        case 'admin':
+          return <AdminPage user={user} vote={vote} logout={logout} />;
+        default:
+          return <div>Unknown View</div>;
+      }
     return(
         <div>
 
