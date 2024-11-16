7be13bd5ba6345e8e2d63056b5e6089b219b6d3c
Wisambebar1 <wisambebar@gmail.com>
2024-11-16 12:21:43 +0200

    applied context hook to app.jsx and start to work on main app

--- voting_app/src/App.jsx
+++ voting_app/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
+import { UserContext } from './Components/Context'
+import Main_app from "./Components/Main_app"
 function App() {
   return (
     <div>
+      <UserContext.Provider>
+        <Main_app/>
+      </UserContext.Provider>
     </div>
   )
 }
